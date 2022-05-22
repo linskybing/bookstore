7f16b5a16f1aee79a89c78f3fda357c67574c1d8
linskybing <linskybing@gmail.com>
2022-05-23 03:48:18 +0800

    0523

---
+++ js/api/product.js
@@ -0,0 +1,45 @@
+var p_nowpage = 1;
+const p_itemnum = 10;
+function GetSellerProduct(state, search) {
+    var token = getCookie('token');
+    return fetch(apidomain + '/products/' + state + '/' + search + '/' + p_nowpage + '/' + p_itemnum, {
+        method: 'GET',
+        headers: {
+            'Authorization': token,
+        }
+    })
+        .then(res => res.json())
+        .then(res => {
+            return res;
+        })
+        .catch(e => {
+            console.error('Error:', error)
+        })
+}
+
+function UpdateProductInfo(data, id) {
+    const token = getCookie('token')
+    var formBody = []
+    var details = data
+    for (var property in details) {
+        var encodedKey = encodeURIComponent(property)
+        var encodedValue = encodeURIComponent(details[property])
+        formBody.push(encodedKey + "=" + encodedValue)
+    }
+    return fetch(apidomain + '/product/' + id, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'Authorization': token,
+        },
+        body: formBody.join('&')
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            return data
+        })
+        .catch(e => {
+            console.error('Error:', e)
+        })
+}
--- js/profile/forproduct.js
+++ js/profile/forproduct.js
@@ -1,5 +1,21 @@
 
-var prodcutlist;
+var prodcutonlist;
+var onIndex = 0;
+var prodcutofflist;
+var offIndex = 0;
+var replenishment;
+var repIndex = 0;
+var nowIndex = 0;
+var nowType = 'on';
+var tempdata = {}
+
+//載入商品資運
+function loadonproduct() {
+    GetOnProduct('null');
+
+}
+
+
 
 let checkbox_all = document.querySelector('.checkbox_head input');
 checkbox_all.addEventListener('click', function () {
@@ -26,11 +42,6 @@ insert.addEventListener('click', function () {
     displaymodal();
 })
 
-let edit = document.querySelector('#editbtn');
-edit.addEventListener('click', function () {
-    modal.classList.toggle('hidden');
-    editmodal();
-})
 
 //顯示modal內容
 let modal_content = document.querySelector('.modal-content-2')
@@ -80,9 +91,27 @@ function displaymodal() {
     closemodal();
     modal_content_event('.modal .modal-content-2');
 }
+//取得資料陣列
+function GetDataArray(type) {
+    var data;
+    if (type == 'on') {
+        data = prodcutonlist;
+    }
+    else if (type == 'off') {
+        data = prodcutofflist;
+    }
+    else {
+        data = replenishment;
+    }
+    return data;
+}
 
 //修改資訊modal
-function editmodal() {
+function editmodal(id, type) {
+    var data;
+    data = GetDataArray(type);
+    nowIndex = id;
+    nowType = type;
     modal.innerHTML = `
     <div class="modal-content-2">
         <div class="header">
@@ -101,25 +130,33 @@ function editmodal() {
             </div>
         </div>
         <div class="content">
-            <div class="formgroup">
-                <label for="editname">商品名稱</label>
-                <input type="text" id="editname">
+            <div class="formerror">                    
             </div>
             <div class="formgroup">
-                <label for="editprice">商品價格</label>
-                <input type="text" id="editprice">
+                <label for="editname">商品名稱<span class="must">*</span></label>
+                <input type="text" id="editname" value="${data[id].Name}">
             </div>
             <div class="formgroup">
-                <label for="store">　　庫存</label>
-                <input type="text" id="store">
+                <label for="editprice">商品價格<span class="must">*</span></label>
+                <input type="text" id="editprice" value="${data[id].Price}">
             </div>
             <div class="formgroup">
-                <label for="description">商品敘述</label>
-                <textarea id="description"></textarea>
+                <label for="store">　　庫存<span class="must">*</span></label>
+                <input type="text" id="store" value="${data[id].Inventory}">
+            </div>
+            <div class="formgroup">
+                <label for="description">商品敘述<span class="must">*</span></label>
+                <textarea id="description">${data[id].Description}</textarea>
             </div>            
         </div>
         <div class="footer">
-            <button class="submit">
+            <button class="submit now" id="submit_p">
+                更新資料
+            </button>
+            <button class="submit hidden" id="submit_r">
+                更新資料
+            </button>
+            <button class="submit hidden" id="submit_i">
                 更新資料
             </button>
             <span class="cancel">取消</span>
@@ -131,6 +168,105 @@ function editmodal() {
     modal_content_event('.modal .modal-content-2');
     modal_rent_info();
     modal_image_info();
+    sendeditdata();
+}
+
+//送出修改資料
+function sendeditdata() {
+    let btn = document.querySelector('#submit_p');
+    btn.addEventListener('click', async function () {
+        var data;
+        data = GetDataArray(nowType);
+        let Name = document.querySelector('#editname').value;
+        let Price = document.querySelector('#editprice').value;
+        let Inventory = document.querySelector('#store').value;
+        let Description = document.querySelector('#description').value;
+        tempdata = Object.assign(tempdata, { 'Name': Name });
+        tempdata = Object.assign(tempdata, { 'Price': Price });
+        tempdata = Object.assign(tempdata, { 'Inventory': Inventory });
+        tempdata = Object.assign(tempdata, { 'Description': Description });
+        if (!checkRate(parseInt(tempdata.Price)) || !checkRate(parseInt(tempdata.Inventory))) {
+            numbererror()
+        }
+        else if (validate(tempdata)) {
+            UpdateInfo(tempdata);
+        }
+        else {
+            document.querySelector('.formerror').classList.add('erroractive');
+            document.querySelector('.formerror').innerHTML = '必填資料不可為空';
+        }
+
+    })
+    document.getElementById('submit_r').addEventListener('click', async function () {
+        let isrent = document.getElementById('rentbox');
+        if (isrent.checked) {
+            let maxrent = document.querySelector('#editrent').value;
+            let rentpriceedit = document.querySelector('#rentprice').value;
+            tempdata = Object.assign(tempdata, { 'Rent': 1 });
+            tempdata = Object.assign(tempdata, { 'MaxRent': maxrent });
+            tempdata = Object.assign(tempdata, { 'RentPrice': rentpriceedit });
+            if (!checkRate(maxrent) || !checkRate(rentpriceedit)) {
+                document.querySelector('.formerror').classList.add('erroractive');
+                document.querySelector('.formerror').innerHTML = '數量欄位必須為正整數且大於0';
+            }
+            else {
+                UpdateInfo(tempdata);
+            }
+        } else {
+            let maxrent = document.querySelector('#editrent').value;
+            let rentpriceedit = document.querySelector('#rentprice').value;
+            tempdata = Object.assign(tempdata, { 'Rent': 0 });
+            tempdata = Object.assign(tempdata, { 'MaxRent': 0 });
+            tempdata = Object.assign(tempdata, { 'RentPrice': 0 });
+            UpdateInfo(tempdata);
+
+        };
+    })
+}
+
+//送出資訊
+async function UpdateInfo(tempdata) {
+    var data;
+    data = GetDataArray(nowType);
+    document.querySelector('.formerror').classList.remove('erroractive');
+    document.querySelector('.formerror').innerHTML = '';
+    var re;
+    await UpdateProductInfo(tempdata, data[nowIndex].ProductId).then(r => re = r);
+    if (re.hasOwnProperty('error')) {
+        document.querySelector('.formerror').classList.add('erroractive');
+        document.querySelector('.formerror').innerHTML = re.error;
+    }
+    else {
+        document.querySelector('.formerror').classList.add('succece');
+        document.querySelector('.formerror').innerHTML = re.info;
+        GetOnProduct('null');
+        setTimeout(() => {
+            document.querySelector('.formerror').classList.remove('succece');
+        }, 3000);
+    }
+}
+
+//數量級錯
+function numbererror() {
+    document.querySelector('.formerror').classList.add('erroractive');
+    document.querySelector('.formerror').innerHTML = '數量欄位必須為正整數且大於0';
+}
+
+//驗證資料是否為空
+function validate(data) {
+    var btn = true;
+    Object.entries(data).forEach(([key, value]) => {
+        if (value.length == 0) btn = false;
+    })
+    return btn;
+}
+
+//判斷正整數
+function checkRate(nubmer) {
+    if (/^[0-9]*[1-9][0-9]*$/.test(nubmer))
+        return true;
+    else
+        return false;
 }
 
 //關閉modal
@@ -157,26 +293,38 @@ function modal_content_event(type) {
 function modal_rent_info() {
     let rent = document.getElementById('rent');
     rent.addEventListener('click', function () {
+        var data;
+        data = GetDataArray(nowType);
+        let now = document.querySelector('.now');
+        now.classList.remove('now');
+        now.classList.add('hidden');
+        let rentbtn = document.getElementById('submit_r')
+        rentbtn.classList.remove('hidden');
+        rentbtn.classList.add('now');
         let content = document.querySelector('.modal .content');
         content.innerHTML = `
+        <div class="formerror">                    
+        </div>
         <div class="formgroup onoption">
         <label>租借功能　　</label>
-        <input type="checkbox" id="rent">
-        <label class="toggle" for="rent">
-            <div class="radio default"></div>
+        <input type="checkbox" id="rentbox" >
+        <label class="toggle" for="rentbox">
+            <div class="radio ${(data[nowIndex].Rent == 0) ? "default" : "default-2"}"></div>
         </label>                   
         </div>
         <div class="formgroup">
             <label for="editrent">最大租借天數</label>
-            <input type="text" id="editrent">
+            <input type="text" id="editrent" ${(data[nowIndex].Rent == 0) ? 'disabled="disabled"' : ''} value="${data[nowIndex].MaxRent}">
         </div>
         <div class="formgroup">
             <label for="rentprice">租借價格(天)&nbsp;</label>
-            <input type="text" id="rentprice">
+            <input type="text" id="rentprice" ${(data[nowIndex].Rent == 0) ? 'disabled="disabled"' : ''} value="${data[nowIndex].RentPrice}">
         </div>
         `;
         radio_event();
         modal_product_info();
+        let rentbox = document.getElementById('rentbox');
+        rentbox.checked = (data[nowIndex].Rent != 0);
     })
 }
 
@@ -191,18 +339,43 @@ function radio_event() {
     rent.classList.add('active');
     option.addEventListener('click', function () {
         let radio = document.querySelector('.onoption .toggle .radio');
+        let rentbox = document.querySelector('#rentbox');
         if (btn) {
             btn = false;
-            radio.classList.remove('default');
-        }
+            if (!rentbox.checked) {
+                radio.classList.remove('default');
+                radio.classList.add('toggle-check');
+                let modalinput = document.querySelectorAll('.modal input[type=text]');
+                modalinput.forEach(e => {
+                    e.removeAttribute('disabled');
+                })
+                return false;
+            }
+            else {
+                radio.classList.remove('default-2');
+                radio.classList.add('toggle-uncheck');
+                let modalinput = document.querySelectorAll('.modal input[type=text]');
+                modalinput.forEach(e => {
+                    e.setAttribute('disabled', 'disabled');
+                })
+                return false;
+            }
 
-        let radio2 = document.querySelector('.toggle .toggle-check');
-        if (radio2) {
-            radio.classList.toggle('toggle-uncheck');
+        }
+        if (rentbox.checked) {
+            let modalinput = document.querySelectorAll('.modal input[type=text]');
+            modalinput.forEach(e => {
+                e.setAttribute('disabled', 'disabled');
+            })
         }
         else {
-            radio.classList.toggle('toggle-check');
+            let modalinput = document.querySelectorAll('.modal input[type=text]');
+            modalinput.forEach(e => {
+                e.removeAttribute('disabled');
+            })
         }
+        radio.classList.toggle('toggle-uncheck');
+        radio.classList.toggle('toggle-check');
 
     });
 }
@@ -212,23 +385,33 @@ function radio_event() {
 function modal_product_info() {
     let product = document.getElementById('product');
     product.addEventListener('click', function () {
+        var data;
+        data = GetDataArray(nowType);
+        let now = document.querySelector('.now');
+        now.classList.remove('now');
+        now.classList.add('hidden');
+        let btn = document.getElementById('submit_p')
+        btn.classList.remove('hidden');
+        btn.classList.add('now');
         let content = document.querySelector('.modal .content');
         content.innerHTML = `
+            <div class="formerror">                    
+            </div>
             <div class="formgroup">
                 <label for="editname">商品名稱</label>
-                <input type="text" id="editname">
+                <input type="text" id="editname" value="${data[nowIndex].Name}">
             </div>
             <div class="formgroup">
                 <label for="editprice">商品價格</label>
-                <input type="text" id="editprice">
+                <input type="text" id="editprice" value="${data[nowIndex].Price}">
             </div>
             <div class="formgroup">
                 <label for="store">　　庫存</label>
-                <input type="text" id="store">
+                <input type="text" id="store" value="${data[nowIndex].Inventory}">
             </div>
             <div class="formgroup">
                 <label for="description">商品敘述</label>
-                <textarea id="description"></textarea>
+                <textarea id="description">${data[nowIndex].Description}</textarea>
             </div> 
         `;
         modal_product_event();
@@ -247,9 +430,19 @@ function modal_product_event() {
 function modal_image_info() {
     let photo = document.getElementById('photo');
     photo.addEventListener('click', function () {
+        var data;
+        data = GetDataArray(nowType);
+        let now = document.querySelector('.now');
+        now.classList.remove('now');
+        now.classList.add('hidden');
+        let btn = document.getElementById('submit_i')
+        btn.classList.remove('hidden');
+        btn.classList.add('now');
         imgcount = 1;
         let content = document.querySelector('.modal .content');
         content.innerHTML = `
+            <div class="formerror">                    
+            </div>
             <div class="formgroup">
                 <label>商品圖片</label>
             </div>
@@ -328,3 +521,91 @@ function createfileitem() {
     let image = document.getElementById('photo_' + imgcount);
     image.addEventListener('change', view_upload_image);
 }
+
+
+//載入商品陣列
+async function GetOnProduct(search) {
+    var data;
+    await GetSellerProduct('on', search).then(r => data = r);
+    if (data.hasOwnProperty('data')) {
+        prodcutonlist = data.data;
+    }
+    ClearContent();
+    prodcutonlist.forEach(element => {
+        let div = document.createElement('div');
+        div.classList.add('table_content');
+        div.innerHTML = `   
+            <div class="checkbox">
+                <input type="checkbox" id="p_${element.ProductId}" name="product" value="${element.ProductId}">
+                <label for="p_${element.ProductId}"><i class="fa-solid fa-check"></i></label>
+            </div>
+            <div class="productimg">
+                <img src="${(element.Image != null) ? "http://localhost:8080/images/Products/" + element.Image[0].Image : ""}" alt="NULL">
+            </div>
+            <div class="productname">
+                ${element.Name}
+            </div>
+            <div class="productprice">
+            ${element.Price} NT
+            </div>
+            <div class="store">
+            ${element.Inventory}
+            </div>
+            <div class="product_action">
+                <input type="hidden" name="" id="index_${element.ProductId}" value="${onIndex}">
+                <span id="editbtn_${onIndex}"><i class="fa-solid fa-pen-to-square"></i></span>
+                <span id="deletebtn_${onIndex}"><i class="fa-solid fa-trash-can"></i></span>
+            </div>
+         `;
+        document.querySelector('.product_table').appendChild(div);
+        let edit = document.querySelector('#editbtn_' + onIndex);
+        edit.addEventListener('click', function () {
+            modal.classList.toggle('hidden');
+            var getindex = document.getElementById('index_' + element.ProductId);
+            editmodal(getindex.value, 'on');
+        })
+        onIndex += 1;
+    });
+
+}
+
+function ClearContent() {
+    onIndex = 0;
+    document.querySelector('.product_table').innerHTML = `
+                    <div class="table_action">
+                        <div class="h1"></a></div>
+                        <div class="action">
+                            <button class="insert">
+                                新增商品
+                            </button>
+                            <button class="delete">
+                                下架商品
+                            </button>
+                        </div>
+                    </div>
+                    <div class="table_thead">
+                        <div class="checkbox_head">
+                            <input type="checkbox" id="p_all">
+                            <label for="p_all"><i class="fa-solid fa-check"></i></label>
+                        </div>
+                        <div class="productimg_head">
+                            商品圖片
+                        </div>
+                        <div class="productname_head">
+                            商品名稱
+                        </div>
+                        <div class="productprice_head">
+                            價格
+                        </div>
+                        <div class="store_head">
+                            剩餘庫存
+                        </div>
+                        <div class="prodcut_action_head">
+                            操作
+                        </div>
+                    </div>
+    `;
+}
+
+
+GetOnProduct('null')
