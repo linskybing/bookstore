69163435eb5e0235294efcb77c71bfb536ba69e0
linskybing <linskybing@gmail.com>
2022-05-26 15:32:06 +0800

    0526

--- js/api/dealrecord.js
+++ js/api/dealrecord.js
@@ -33,3 +33,72 @@ function GetDealRecordS() {
         })
 
 }
+
+function UpdateDealRecord(id, data) {
+    const token = getCookie('token');
+    var formBody = [];
+    var details = data;
+    for (var property in details) {
+        var encodedKey = encodeURIComponent(property)
+        var encodedValue = encodeURIComponent(details[property])
+        formBody.push(encodedKey + "=" + encodedValue)
+    }
+    return fetch(apidomain + '/dealr/' + id, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'Authorization': token,
+        },
+        body: formBody.join('&')
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            return data
+        })
+        .catch(e => {
+            console.error('Error:', e)
+        })
+
+}
+
+function CreateDealMessage(id, content) {
+    const token = getCookie('token');
+    var data = new FormData();
+    data.append('RecordId', id);
+    data.append('Content', content);
+    return fetch(apidomain + '/dealm', {
+        method: 'POST',
+        headers: {
+            'Authorization': token,
+        },
+        body: data,
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            return res;
+        })
+        .catch(e => {
+            console.error('Error:', error)
+        })
+
+
+}
+
+function ReadCancel(id, user) {
+    var token = getCookie('token');
+    return fetch(apidomain + '/readreson/' + id + '/' + user, {
+        method: 'GET',
+        headers: {
+            'Authorization': token,
+        }
+    })
+        .then(res => res.json())
+        .then(res => {
+            return res;
+        })
+        .catch(e => {
+            console.error('Error:', error)
+        })
+}
