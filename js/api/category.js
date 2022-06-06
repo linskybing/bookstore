be78d37eac9d3c3a5608c5ce76ad0e2e149f8032
linskybing <83772717+linskybing@users.noreply.github.com>
2022-06-06 23:22:44 +0800

    0606

--- js/api/category.js
+++ js/api/category.js
@@ -84,3 +84,72 @@ function RecordForChart() {
             console.error('Error:', error)
         })
 }
+
+// category
+
+function PATCHETag(id, data) {
+    const token = getCookie('token');
+    var formBody = [];
+    var details = data;
+    for (var property in details) {
+        var encodedKey = encodeURIComponent(property)
+        var encodedValue = encodeURIComponent(details[property])
+        formBody.push(encodedKey + "=" + encodedValue)
+    }
+    return fetch(apidomain + '/category/' + id, {
+        method: 'PATCH',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+            'Authorization': token,
+        },
+        body: formBody.join('&')
+    })
+        .then(res => res.json())
+        .then(data => {
+            console.log(data)
+            return data
+        })
+        .catch(e => {
+            console.error('Error:', e)
+        })
+}
+
+function CreateTag(tag, color) {
+    const token = getCookie('token');
+    var data = new FormData();
+    data.append('Tag', tag);
+    data.append('Color', color);
+    return fetch(apidomain + '/category', {
+        method: 'POST',
+        headers: {
+            'Authorization': token,
+        },
+        body: data,
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            return res;
+        })
+        .catch(e => {
+            console.error('Error:', error)
+        })
+}
+
+function DeleteTag(id) {
+    const token = getCookie('token');
+    return fetch(apidomain + '/category/' + id, {
+        method: 'DELETE',
+        headers: {
+            'Authorization': token,
+        },
+    })
+        .then(res => res.json())
+        .then(res => {
+            console.log(res);
+            return res;
+        })
+        .catch(e => {
+            console.error('Error:', error)
+        })
+}
